265b1f819068e269b9b667b864f0ee6d8ee816a6
Livia Lima <86426223+livialim@users.noreply.github.com>
2021-12-09 11:03:00 -0300

    Initial commit

--- arrays-number.js
+++ arrays-number.js
@@ -43,15 +43,54 @@ console.log(exibeNotas("Liz"))
 
 console.log("****************Imprimir com for***********************")
 
+//vai imprimir os números com os respectivos indíces
 const nums = [100, 200, 300, 400, 500, 600];
 // o loop vai acontecer enquanto o valor de i for menor que 6
 for (let i = 0; i < nums.length; i++){  // ++ aumenta em 1 o valor da variável i (i = i + 1)
     console.log(i, nums[i]) 
 }
-
-console.log("\nExplicação:\ncondição de parada i <= 5 está correta, a variável i que faz o controle do loop começa em 0 e ao fim de cada ciclo do loop,\né aumentada em 1 (i++). Quando o valor da variável de controle i chega a 6, depois de imprimir o número 5 no terminal,\na comparação i <= 5 retorna false e isso encerra o loop.\n")
+//Explicação: condição de parada i <= 5 está correta, a variável i que faz o controle do loop começa em 0 e ao fim de cada ciclo do loop, é aumentada em 1 (i++). Quando o valor da variável de controle i chega a 6, depois de imprimir o número 5 no terminal, a comparação i <= 5 retorna false e isso encerra o loop.
 
 for (let i = 0; i <= 5; i++){
     console.log(i)
 }
 
+console.log("***********************forEach************************")
+
+const nums = [10, 6.5, 8, 7.5];
+let soma = 0;
+// nums.forEach(nota => {
+//     soma += nota
+// } )
+nums.forEach(function(nota){ // no forEach temos uma função que recebe um elemento
+    soma += nota
+} )
+
+let media = soma / nums.length 
+
+console.log(`A media é ${media}`)
+
+console.log("************************map*****************************")
+
+const array = [10, 9, 8, 7, 6];
+
+const atualizacao = array.map(nota => nota + 1) // map() atualiza a array "da novos valores"
+console.log(atualizacao)
+
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+const upper = nomes.map(nomes => nomes.toUpperCase()); //toUpperCase é um método, métodos tem () vazio
+console.log(upper) 
+
+console.log("**************************filter***************************")
+
+const nomes = ["Ana", "Pedro", "Maria", "Mauro"];
+const notas = [7, 4.5, 8, 7.5];
+
+const reprovados = nomes.filter((_, indice) => notas [indice] < 6); // "_" mostra que tem um parâmetro mas n está sendo usado
+console.log(`Reprovados: ${reprovados}`)
+
+console.log("*************************reduce***************************")
+//método reduce() “reduzir” uma lista de números para um único valor.
+const notas = [7, 4.5, 8, 7.5];
+const media = notas.reduce((acum, atual) => atual + acum, 0) / notas.length
+console.log(`A média é: ${media}`)
