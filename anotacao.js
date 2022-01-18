90ba79de186e0176107fef690448c053338d463f
Livia Lima <86426223+livialim@users.noreply.github.com>
2022-01-17 21:30:38 -0300

    atualiza

--- anotacao.js
+++ anotacao.js
@@ -1,10 +1,22 @@
-console.log("deu erro");
 console.error(new Error("deu erro"));
 
 const idadeMinima = 18;
 const idadeCliente = 16;
             //condição                    //true     //false
 console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
+
+// JSON
+// É um formato que utiliza a sintaxe de objetos e arrays do JavaScript.
+//JSON é um formato criado para transferência de dados, sendo assim é necessário convertê-lo para um objeto JavaScript para que os dados possam ser utilizados em um programa. Para isso, existem dois métodos nativos:
+// ** JSON.parse(): converte JSON para um objeto JavaScript;
+// ** JSON.stringify(): converte um objeto JavaScript para o formato JSON.
+
+// As diferenças de sintaxe entre JSON e um objeto JavaScript são:
+
+// No JSON, as chaves sempre devem estar entre aspas duplas ” ”, no formato string. Já no objeto JavaScript, as aspas não são obrigatórias;
+// O JSON aceita como valores apenas dados primitivos (string, number para números finitos, true, false e null), objetos e arrays. Não é possível declarar funções/métodos;
+// Não são permitidas vírgulas após o último conjunto de chave/valor do objeto.
+
 
 // arguments
 // as
